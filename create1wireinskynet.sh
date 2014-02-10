956f0f93c0bc95a12d28b7a81365e388b1f9e8dd
Patrik Nordling <patrik.nordling@gmail.com>
2014-02-10 20:47:49 +0100

    fixes

--- create1wireinskynet.sh
+++ create1wireinskynet.sh
@@ -2,23 +2,23 @@
 #
 # target is a raspberry
 #
-1WIRE=/mnt/1wire
+ONEWIRE=/mnt/1wire
 SKYNET=http://localhost:3000/devices
 
 #set -x
 
-for dev in $1WIRE/1* ; do
+for dev in ${ONEWIRE}/1* ; do
 	family=`cat $dev/family`
 	id=`cat $dev/id`
 	devtype=`cat $dev/type`
 
-	if -f $dev/temperature ; then
+	if [ -f $dev/temperature ] ; then
 		function="temperature"
-	elif -f $dev/counters.ALL ; then
-		function="counter"		
+	elif [ -f $dev/counters.ALL ] ; then
+		function="counter"
 	fi
-	
-	args='"uuid=${family}.${if}&devicetype=${devtype}&function=${function}"'
+
+	args="uuid=${family}.${id}&devicetype=${devtype}&function=${function}"
 
 	echo $args
 
