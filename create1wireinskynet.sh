04a1bbb22eefc71cc249d2c0abf02c4af1b8088a
Patrik Nordling <nils.patrik@gmail.com>
2014-02-10 20:37:58 +0100

    test

--- create1wireinskynet.sh
+++ create1wireinskynet.sh
@@ -2,3 +2,24 @@
 #
 # target is a raspberry
 #
+1WIRE=/mnt/1wire
+SKYNET=http://localhost:3000/devices
+
+#set -x
+
+for dev in $1WIRE/1* ; do
+	family=`cat $dev/family`
+	id=`cat $dev/id`
+	devtype=`cat $dev/type`
+
+	if -f $dev/temperature ; then
+		function="temperature"
+	elif -f $dev/counters.ALL ; then
+		function="counter"		
+	fi
+	
+	args='"uuid=${family}.${if}&devicetype=${devtype}&function=${function}"'
+
+	echo $args
+
+done
